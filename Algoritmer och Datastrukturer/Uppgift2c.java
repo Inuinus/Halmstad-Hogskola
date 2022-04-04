3ca09c32e96cdb96a97e3021a6a28999a62e292d
Inuinus <linusfrisk01@gmail.com>
2022-04-04 11:05:14 +0200

    update before handin

--- Algoritmer och Datastrukturer/Uppgift2c.java
+++ Algoritmer och Datastrukturer/Uppgift2c.java
@@ -2,36 +2,38 @@ import java.util.Comparator;
 
 public class Uppgift2c{
     public static MyArrayList<Land> findMinMax(MyArrayList<Land> list){
-        Land biggest = list.get(0);
-        Land smallest = list.get(0);
+        Land minst = list.get(0);
+        Land storst = list.get(0);
+
         for (int i = 1; i < list.size; i++) {
-            if(list.get(i).compareTo(biggest) > 0){   // compareTo jämför ju beroende på vilken datatyp elementen är...
-                biggest = list.get(i);  // alltså måste vi på uppgift 2b skapa 3 helt olika datatyper?
+            if(list.get(i).compareTo(storst) > 0){ 
+                storst = list.get(i); 
             }
-            if((list.get(i).compareTo(smallest) < 0)){
-                smallest = list.get(i);
+            if((list.get(i).compareTo(minst) < 0)){
+                minst = list.get(i);
             }
         }
         MyArrayList<Land> pair = new MyArrayList<>();
-        pair.add(smallest);
-        pair.add(biggest);
+        pair.add(minst);
+        pair.add(storst);
         return pair;
     }
 
     public static MyArrayList<Land> findMinMax(MyArrayList<Land> list, Comparator<Land> c){
-        Land biggest = list.get(0);
-        Land smallest = list.get(0);
+        Land minst = list.get(0);
+        Land storst = list.get(0);
+
         for (int i = 1; i < list.size; i++) {
-            if(c.compare(list.get(i),biggest) > 0){   // compareTo jämför ju beroende på vilken datatyp elementen är...
-                biggest = list.get(i);  // alltså måste vi på uppgift 2b skapa 3 helt olika datatyper?
+            if(c.compare(list.get(i),storst) > 0){ 
+                storst = list.get(i); 
             }
-            if(c.compare(list.get(i),smallest) < 0){
-                smallest = list.get(i);
+            if(c.compare(list.get(i),minst) < 0){
+                minst = list.get(i);
             }
         }
         MyArrayList<Land> pair = new MyArrayList<>();
-        pair.add(smallest);
-        pair.add(biggest);
+        pair.add(minst);
+        pair.add(storst);
         return pair;
     }
 
