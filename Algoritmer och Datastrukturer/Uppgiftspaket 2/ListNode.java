cfee58f36879d1edc3717aa007fafdc87f9e27b1
Inuinus <linusfrisk01@gmail.com>
2022-04-05 20:18:20 +0200

    upd

---
+++ Algoritmer och Datastrukturer/Uppgiftspaket 2/ListNode.java
@@ -0,0 +1,4 @@
+public class ListNode<T>{
+    // Denna klass ska innehålla tre instansvariabler som representerar värdet(av typen T) och
+    // referenser till en föregående och efterföljande nod.
+}
