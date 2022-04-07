ef336952f098d0331b335acae78dc007a18d7e8a
Inuinus <linusfrisk01@gmail.com>
2022-04-07 15:22:54 +0200

    Update DoublyLinkedList.java

--- Algoritmer och Datastrukturer/Uppgiftspaket 2/DoublyLinkedList.java
+++ Algoritmer och Datastrukturer/Uppgiftspaket 2/DoublyLinkedList.java
@@ -9,6 +9,19 @@ public class DoublyLinkedList<T extends Comparable<T>> implements Iterable<T>{
         size = 0;
     }
 
+    //Lägg till t i slutet av listan.
+    public void add(T t){
+        if(isEmpty()){
+            head = new DoublyLinkedList<T>(t, null);
+            size++;
+            return;
+        }
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
     public Iterator<T> iterator() {
 
         return null;
