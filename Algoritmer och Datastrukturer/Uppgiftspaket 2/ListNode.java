c4f3462dc6065f0ee151bf971d90b8cefc83efde
Inuinus <linusfrisk01@gmail.com>
2022-04-07 14:32:28 +0200

    ListNode

--- Algoritmer och Datastrukturer/Uppgiftspaket 2/ListNode.java
+++ Algoritmer och Datastrukturer/Uppgiftspaket 2/ListNode.java
@@ -1,4 +1,31 @@
 public class ListNode<T>{
     // Denna klass ska innehålla tre instansvariabler som representerar värdet(av typen T) och
     // referenser till en föregående och efterföljande nod.
+    T value;
+    ListNode<T> previous;
+    ListNode<T> next;
+    
+    public ListNode(T value, ListNode<T> next, ListNode<T> previous){
+        this.value = value;
+        this.next = next;
+        this.previous = previous;
+    }
+
+    public ListNode(T value, ListNode<T> next){
+        this.value = value;
+        this.next = next;
+        this.previous = null;
+    }
+
+    public ListNode(T value){
+        this.value = value;
+        this.next = null;
+        this.previous = null;
+    }
+
+    public ListNode(){
+        this.value = null;
+        this.next = null;
+        this.previous = null;
+    }
 }
