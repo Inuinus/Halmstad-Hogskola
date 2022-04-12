5bf4be840cf8790fd69c3765be1227b8633abd01
Inuinus <linusfrisk01@gmail.com>
2022-04-12 14:33:05 +0200

    Fix of 99->100

--- DAT-Labs/Lab1_2/asm(2).s
+++ DAT-Labs/Lab1_2/asm(2).s
@@ -11,13 +11,13 @@ LOOP    CMP R2, # 99    ; compare (R2) to 99
         BNE CMP0        ; different > jump to CMP0
         MOV R1, # 0     ; equal > restart R1
         MOV R2, # 0     ; equal > restart R2
-        ADD R0, R0, # 7 ; update first: (R0) < (R0) + 7 
+        ADD R0, R0, # 103 ; update first: (R0) < (R0) + 103 
         B LOOPE         ; jump to LOOPE
-CMP0    CMP R1, # 9     ; R1 < 9 diffenrent > increase R0,R1,R2 by 1
+CMP0    CMP R1, # 9     ; R1 < 9 diffenrent > increase R0,R1,R2 by 1; Compare R1 to 9
         BNE ADD0
         MOV R1, # 0     ; (R0) = 0
         ADD R2, R2, # 1 ; update first: (R2) < (R2) + 1
-        ADD R0, R0, # 13 ; update first: (R0) < (R0) + 13
+        ADD R0, R0, # 7 ; update first: (R0) < (R0) + 7
         B LOOPE
 ADD0    ADD R0, R0, # 1 ; update first: (R0) < (R0) + 1
         ADD R1, R1, # 1 ; update first: (R1) < (R1) + 1
