d0e0b9003d52ca7e550532f89f2ccb4143f88ac9
Inuinus <linusfrisk01@gmail.com>
2022-04-25 11:27:54 +0200

    Update InsertionSort.java

--- Algoritmer och Datastrukturer/Uppgiftspaket 2/InsertionSort.java
+++ Algoritmer och Datastrukturer/Uppgiftspaket 2/InsertionSort.java
@@ -4,11 +4,11 @@ public class InsertionSort<T extends Comparable<T>>{
         Comparable[] sortedarray = new Comparable[a.length];
        
        for(int i = 0; i < a.length; i++){
-           lista.addAtFirstSmaller(a[i]);
+           lista.addAtFirstSmaller(a[i]);//insert all the sorted, the elements
        }
         
        for(int i = 0; i < a.length; i++){
-           sortedarray[i] = lista.get(i);
+           sortedarray[i] = lista.get(i);//puts in the sorted order in "sortedarray"
        }
         return sortedarray;
     }
