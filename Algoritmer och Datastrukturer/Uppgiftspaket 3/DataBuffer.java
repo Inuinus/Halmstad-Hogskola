bb76f55edd1ab3005b3644e96e43db8c1604cb57
Inuinus <linusfrisk01@gmail.com>
2022-04-26 13:19:01 +0200

    update DataBuffer

---
+++ Algoritmer och Datastrukturer/Uppgiftspaket 3/DataBuffer.java
@@ -0,0 +1,66 @@
+import java.util.Iterator;
+
+public class DataBuffer<T> implements Iterable<T>{
+    int bufferSize;
+    int size;
+    int front;
+    int back;
+    T[] a;
+
+    public DataBuffer(int bufferSize){
+        if(bufferSize < 1){
+            throw new IllegalAccessError("The size of the buffer cannot be less than 1");
+        }
+        this.bufferSize = bufferSize;
+        this.back = -1;
+        this.front = 0;
+        this.size = 0;
+        this.a = (T[]) new Comparable[bufferSize];
+    }
+
+    public void enqueue(T t){
+        back = (back + 1);
+        a[back] = t;
+        size = size + 1;
+    }
+
+
+
+    @Override
+    public Iterator<T> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    public void printBuffer() {
+        // custom class
+
+        String s = "(";
+        for (T t : a) {
+            s += t + ", ";
+        }
+
+        if (!isEmpty())
+            s = s.substring(0, s.length() - 2);
+        s += ")";
+        System.out.println(s);
+        System.out.println("back: " + this.back);
+        System.out.println("front: " + this.front);
+        System.out.println("size: " + this.size);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        DataBuffer<Integer> buffer1 = new DataBuffer<>(5);
+        buffer1.enqueue(4);
+        buffer1.enqueue(7);
+        buffer1.enqueue(2);
+        buffer1.enqueue(9);
+        buffer1.printBuffer();
+
+    }
+}
