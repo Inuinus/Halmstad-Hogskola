88c38c7afdcde155e0c24037dc8e0806714170b2
Inuinus <linusfrisk01@gmail.com>
2022-04-27 15:26:00 +0200

    update

--- DAT-Labs/Lab2_2/asm.s
+++ DAT-Labs/Lab2_2/asm.s
@@ -1,20 +1,20 @@
-        NAME main
-        PUBLIC main
+         NAME    main
+        PUBLIC  main
         SECTION .text : CODE (2)
         THUMB
 main
-       Mov R2, #0xAC
-       LDR R1, =0xFF554400
-       BL SWAP
-STOP B STOP     ;infinite walk
+      MOV R2, #0xAC
+      LDR R1, =0xFF554400
+      BL SWAP
+STOP B STOP ; infinite walk
 
 SWAP PUSH {R2}
-       LSR R0, R1, # 24
-       AND R2, R1, # 0xFF0000
-       ORR R0, R0, R2, LSR #8
-       AND R2, R1, #0xFF00
-       ORR R0, R0, R2, LSL #8
-       ORR R0, R0, R1, LSL #24
-       POP {R2}
-       BX LR
-END
+      LSR R0, R1, # 24
+      AND R2, R1, # 0xFF0000
+      ORR R0, R0, R2, LSR #8
+      AND R2, R1, #0xFF00
+      ORR R0, R0, R2, LSL #8
+      ORR R0, R0, R1, LSL #24
+      POP {R2}
+      BX LR
+ END 
