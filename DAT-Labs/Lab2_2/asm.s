bb413d38c0cacd273aa52b406b5c8fdad1ded964
Inuinus <linusfrisk01@gmail.com>
2022-05-02 12:18:12 +0200

    update

--- DAT-Labs/Lab2_2/asm.s
+++ DAT-Labs/Lab2_2/asm.s
@@ -2,22 +2,16 @@
         PUBLIC  main
         SECTION .text : CODE (2)
         THUMB
+HamDistReg   
+        CMP R3, R4 ; Compare R3 to R4 
+        BLT r3Less ;if CMP is lesser than jump to r3Less
+        SUB R2, R3, R4  ;Subtract to R2(New Variable) the difference between R3 to R4
+        BX LR           ;BX LR
+r3Less  SUB R2, R4, R3
+        BX LR
 main
-      MOV R0, #0 ;Counter
-      MOV R1, #0 ;Compare this value to the 32-bit number
-      MOV R2, #0 ;The variable that stores what the bit differs in R3 to R4(EOR)
-      LDR R3, =0xAEDFCDEF
-      LDR R4, =0xCEDFACDE
-      BL HamDistReg
-STOP B STOP ; infinite walk
-
-HamDistReg 
-      EOR R1, R3, R4 ;(XOR), Adds the differences (1s) between R3 and R4 to R1
-COUNTER
-      CMP R0, #32 ; Compare the value of R0 to 32-bits, if R0 is 32 the loop stops and BEQ to STOP
-      BEQ STOP
-      LSLS R1, #1 ;Left shift left(S), jumps through the R1 register one at a time
-      ADC R2, #0  ;Adds the carry to r2
-      ADD R0, R0, #1 ;Increase our counter
-      B COUNTER
- END 
+        LDR R3, =0xAEDFCDEF  ; inupt parameter
+        LDR R4, =0xCEDFACDE  ; input parameter
+        BL HamDistReg ; subrutine
+STOP    B STOP ; infinite walk
+        END
