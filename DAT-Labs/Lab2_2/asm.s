c1bc7133c38b34e946f995a20829d783fa3a7155
Inuinus <linusfrisk01@gmail.com>
2022-05-02 11:29:37 +0200

    lab2_2

--- DAT-Labs/Lab2_2/asm.s
+++ DAT-Labs/Lab2_2/asm.s
@@ -3,18 +3,21 @@
         SECTION .text : CODE (2)
         THUMB
 main
-      MOV R2, #0xAC
-      LDR R1, =0xFF554400
-      BL SWAP
+      MOV R0, #0 ;Counter
+      MOV R1, #0 ;Compare this value to the 32-bit number
+      MOV R2, #0 ;The variable that stores what the bit differs in R3 to R4(EOR)
+      LDR R3, =0xAEDFCDEF
+      LDR R4, =0xCEDFACDE
+      BL HamDistReg
 STOP B STOP ; infinite walk
 
-SWAP PUSH {R2}
-      LSR R0, R1, # 24
-      AND R2, R1, # 0xFF0000
-      ORR R0, R0, R2, LSR #8
-      AND R2, R1, #0xFF00
-      ORR R0, R0, R2, LSL #8
-      ORR R0, R0, R1, LSL #24
-      POP {R2}
-      BX LR
+HamDistReg 
+      EOR R1, R3, R4 ;(XOR), Adds the differences (1s) between R3 and R4 to R1
+COUNTER
+      CMP R0, #32 ; Compare the value of R0 to 32-bits, if R0 is 32 the loop stops and BEQ to STOP
+      BEQ STOP
+      LSLS R1, #1 ;Left shift left(S), jumps through the R1 register one at a time
+      ADC R2, #0  ;Adds the carry to r2
+      ADD R0, R0, #1 ;Increase our counter
+      B COUNTER
  END 
