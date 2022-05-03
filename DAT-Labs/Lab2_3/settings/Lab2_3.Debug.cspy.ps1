be13aa4302df7f9533f69f5290fc13169a67618a
Inuinus <linusfrisk01@gmail.com>
2022-05-03 10:07:05 +0200

    Create Lab2_3

--- DAT-Labs/Lab2_3/settings/Lab2_3.Debug.cspy.ps1
+++ DAT-Labs/Lab2_3/settings/Lab2_3.Debug.cspy.ps1
@@ -23,9 +23,9 @@
 
 if ($debugfile -eq "")
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab1_1\settings\Lab1_1.Debug.general.xcl" --backend -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab1_1\settings\Lab1_1.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab2_3\settings\Lab2_3.Debug.general.xcl" --backend -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab2_3\settings\Lab2_3.Debug.driver.xcl" 
 }
 else
 {
-& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab1_1\settings\Lab1_1.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab1_1\settings\Lab1_1.Debug.driver.xcl" 
+& "C:\Program Files\IAR Systems\Embedded Workbench 9.0\common\bin\cspybat" -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab2_3\settings\Lab2_3.Debug.general.xcl" --debug_file=$debugfile --backend -f "C:\Users\linus\Documents\GitHub\Halmstad-H-gskola\DAT-Labs\Lab2_3\settings\Lab2_3.Debug.driver.xcl" 
 }
