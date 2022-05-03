c56d7f226312a12fdb9c8d6dcb2eb2fdbbe64a5d
Inuinus <linusfrisk01@gmail.com>
2022-05-03 10:37:50 +0200

    .

--- DAT-Labs/Lab2_2/asm.s
+++ DAT-Labs/Lab2_2/asm.s
@@ -1,17 +1,26 @@
-         NAME    main
+        NAME    main
         PUBLIC  main
         SECTION .text : CODE (2)
         THUMB
-HamDistReg   
-        CMP R3, R4 ; Compare R3 to R4 
-        BLT r3Less ;if CMP is lesser than jump to r3Less
-        SUB R2, R3, R4  ;Subtract to R2(New Variable) the difference between R3 to R4
-        BX LR           ;BX LR
-r3Less  SUB R2, R4, R3 ; The difference of the two variables is found via subtraction between them two
-        BX LR
+  
 main
-        LDR R3, =0xAEDFCDEF  ; Loading in a Hexadecimal number(32-bit)
-        LDR R4, =0xCEDFACDE  ; Loading in a Hexadecimal number(32-bit)
-        BL HamDistReg ; The subrutine of this program
-STOP    B STOP ; infinite walk
-        END
+     MOV R0, #0 ; counter
+     MOV R1, #0 ; Compare value to our 32-bit number
+     LDR R3, =0xAEDFCDEF
+     LDR R4, =0xCEDFACDE
+     BL HamDistReg
+STOP B STOP ; infinite walk 
+  
+HamDistReg
+     EOR R1, R3, R4
+COUNTER
+     CMP R0, #32 ; compare the value of r0 agains 32 if they are the same the loop stops
+     BEQ FINSIH
+     LSRS R1, #1
+     ADC R2, #0  ;adds the carry to r2
+     ADD R0, R0, #1 ;increases our counter
+     B COUNTER
+     
+FINSIH
+     BX LR
+     END
