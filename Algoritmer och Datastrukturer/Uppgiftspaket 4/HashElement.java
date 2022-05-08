c468730a63a03139167ffcbb82afb2762e100491
Inuinus <linusfrisk01@gmail.com>
2022-05-08 22:53:06 +0200

    Create HashElement.java

---
+++ Algoritmer och Datastrukturer/Uppgiftspaket 4/HashElement.java
@@ -0,0 +1,15 @@
+public class HashElement<Key> implements Comparable<HashElement<Key>>{
+
+
+
+
+
+
+
+    
+    public int compareTo(HashElement that) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
