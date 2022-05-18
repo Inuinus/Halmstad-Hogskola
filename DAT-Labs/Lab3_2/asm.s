9a90c2e9a1dca81c3aefbecb6e037c7c4365e415
Inuinus <linusfrisk01@gmail.com>
2022-05-18 10:34:32 +0200

    Update asm.s

--- DAT-Labs/Lab3_2/asm.s
+++ DAT-Labs/Lab3_2/asm.s
@@ -6,6 +6,7 @@
 
 SysTick_Handler
       MOV R0, #0xFFFFFF01
+      
 L1    ADD R0, #0x1
       SUBS R6, R0, R4 ; R4= 0x0FF
       BLT L1
@@ -16,7 +17,9 @@ main
       MOV R6, #0x0
       MOV R4, #0xFF
       MOV R3, #0x7FFFFFFF
-LOOP  ADD R0, #0x1
+LOOP  
+      ADD R0, #0x1
+      CPSID I
       SUB R6, R0, R3
       CMP R6, #0x0
       BLT LOOP
