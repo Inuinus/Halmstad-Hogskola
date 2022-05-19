edf8262852dcd4b86144785adf29b55df55f2f65
Inuinus <linusfrisk01@gmail.com>
2022-05-19 18:48:20 +0200

    finish

--- Algoritmer och Datastrukturer/Uppgiftspaket 4/Graph.java
+++ Algoritmer och Datastrukturer/Uppgiftspaket 4/Graph.java
@@ -1,12 +1,11 @@
 import java.util.*;
 
 public class Graph {
-
+    //Variables
     public static ArrayList <Graph> nodeArray = new ArrayList<>();
     public static ArrayList <Graph> arrayCopy = new ArrayList<>();
     ArrayList<Graph> neighbour = new ArrayList<>();
     ArrayList<Graph> network = new ArrayList<>();
-
     boolean visited;
     int iX;
     int iY;
@@ -18,18 +17,18 @@ public class Graph {
         this.iY = iY;
         this.iR = iR;
         this.data = data;
-        this.neighbour = new ArrayList<>();
+        this.neighbour = new ArrayList<>();//Make a ArrayList named Neighbour
     }
 
     public Graph(String string) {
     }
 
     public void addNeighbours(Graph neighbourNode){
-        this.neighbour.add(neighbourNode);
+        this.neighbour.add(neighbourNode);//Adds the neightbour node to the ArrayList Neighbour
     }
 
     public void setNeighbours(ArrayList<Graph> neighbours){
-        this.neighbour = neighbours;
+        this.neighbour = neighbours;//we say that neighbour is neighbours
     }
 
     public List<Graph> getNeighbours(){
@@ -46,11 +45,10 @@ public class Graph {
             if(network.size() >= 1){
                 counter++;
             }
-
-            for (int k = 0; k < network.size(); k++) {
-                for (int j = 1; j < arrayCopy.size(); j++)
-                    if (network.contains(arrayCopy.get(j))) {
-                        arrayCopy.remove(j);
+            for (int k = 0; k < network.size(); k++) {//For loop using k = 0 and it does this til we reach the size of network
+                for (int j = 1; j < arrayCopy.size(); j++)//Another For loop but j = 1 and we do this til we reach the copy of the arrays size.
+                    if (network.contains(arrayCopy.get(j))) {//If the network contains the value of the value we get from arrayCopy.
+                        arrayCopy.remove(j);//Remove the value "j"
                     }
                 }
             }
@@ -67,7 +65,6 @@ public class Graph {
 
             if(!value.visited){
                 network.add(value);
-
                 value.visited = true;
             }
 
@@ -113,21 +110,19 @@ public class Graph {
     public int BFS(Graph one, Graph other){//Bredden först sökning
         LinkedList<Integer> queue = new LinkedList<>();
         LinkedList<Integer> res = new LinkedList<>();
-        int jumps = 0;
+        int jumps = 0;//Keep track of how many jumps we do.
         int[] prev = new int[nodeArray.size()];
 
-        if(one.data == other.data)// If it is duplicate, distance is zero
-        {                   
+        if(one.data == other.data){// If it is duplicate, distance is zero                 
             return jumps;
         }
         queue.add(one.data);            //Adds the chosen Node ID to the queue
 
-        for(int i = 0; i < nodeArray.size(); i++)//all elements in prev is set to -1 to represent not visited
-        { 
+        for(int i = 0; i < nodeArray.size(); i++){//all elements in prev is set to -1 to represent not visited 
             prev[i] = -1;
         }
 
-        while(queue.size() != 0){        //Goes throught the queue until it is empty
+        while(queue.size() != 0){//Goes throught the queue until it is empty
             int curri = queue.poll();
 
             for (Graph node : nodeArray.get(curri).getNeighbours()){//Puts the first element from the queue into curr and removes the value from the queue
@@ -135,10 +130,10 @@ public class Graph {
                 if(prev[n] == -1){
                     prev[n] = curri;
                     if(n == other.data){//n becomes the ID of each Node
-                        while(n != one.data){
+                        while(n != one.data){//While n isnt the same af one.data, we addFirst[n] to res.
                             res.addFirst(n);
-                            n = prev[n];
-                            jumps++;
+                            n = prev[n];//n = prev[n]
+                            jumps++;//Add 1 to jumps to keep track of how many jumps we have made.
                         }
                         return jumps;
                     }
--- Algoritmer och Datastrukturer/Uppgiftspaket 4/GraphMain.java
+++ Algoritmer och Datastrukturer/Uppgiftspaket 4/GraphMain.java
@@ -3,29 +3,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class GraphMain {
+public class GraphMain {//Creating the class GraphMain(The Graph StdDraw thingy)
 
-        static boolean overlap(Graph one, Graph other) {
-        double dx = one.iX - other.iX;
-        double dy = one.iY - other.iY;
+        static boolean overlap(Graph one, Graph other) {//One or the other
+        double dx = one.iX - other.iX;//Creating a variable for delta X
+        double dy = one.iY - other.iY;//Creating a variable for delta Y
 
-        if(Math.pow(dx, 2) + Math.pow(dy, 2) <= Math.pow((one.iR + other.iR), 2)){
+        if(Math.pow(dx, 2) + Math.pow(dy, 2) <= Math.pow((one.iR + other.iR), 2)){//pythagorean theorem
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Graph list = new Graph(0,0,0, 0);
+        Graph list = new Graph(0,0,0, 0);//Makes a new graph with nothing in it.
 
-        File nFile = new File(System.getProperty("user.dir") + "\\karta1.txt\\");
+        File nFile = new File(System.getProperty("user.dir") + "\\karta1.txt\\");//Gets the karta txt
         Scanner input = new Scanner(nFile);
 
         StdDraw.setYscale(0, 1000);
         StdDraw.setXscale(0, 1000);
-        StdDraw.setPenColor(Color.BLACK);
+        StdDraw.setPenColor(Color.BLACK);//Sets the pens Xscale, Yscale and PenColour
 
-        while (input.hasNextLine()) {
+        while (input.hasNextLine()) {//the function to get all the inputs(node,x,y,r)
             String node = input.nextLine();
             String x = input.nextLine();
             String y = input.nextLine();
@@ -51,7 +51,7 @@ public class GraphMain {
                 Graph firstNode = Graph.nodeArray.get(i);
                 Graph secondNode = Graph.nodeArray.get(j);
 
-                if (overlap(firstNode, secondNode)) {   //if true
+                if (overlap(firstNode, secondNode)) {//if the overlap is true
                     StdDraw.line(firstNode.iX, firstNode.iY, secondNode.iX, secondNode.iY);
                     StdDraw.setPenColor(Color.PINK);
                     firstNode.addNeighbours(secondNode);
@@ -60,9 +60,9 @@ public class GraphMain {
             }
         }
         //If u wanna check if two nodes are connetected you simply type: System.out.println(Graph.nodeArray.get(nodenumber).isNetwrok(Graph.nodeArray.get(nodenumber)));
-        //To check the amount of connected networks type: System.out.println(list.amountOfNetworks());
-        //To check the longest distance between a node and the furthest one away in the network type: System.out.println(list.distanceBetween(Graph.nodeArray.get(NODENUMBER)));
-        //To check the shortest path between two nodes in a netwrok type: System.out.println(list.BFS(Graph.nodeArray.get(NODENUMBER), Graph.nodeArray.get(NODENUMBER)));
+        //If you want to try to check the amount of connected networks you simply type: System.out.println(list.amountOfNetworks());
+        //If you want to try to check the longest distance between a node in the network you type: System.out.println(list.distanceBetween(Graph.nodeArray.get(NODENUMBER)));
+        //To check the shortest path between two nodes in a network type: System.out.println(list.BFS(Graph.nodeArray.get(NODENUMBER), Graph.nodeArray.get(NODENUMBER)));
 
     }
 }
